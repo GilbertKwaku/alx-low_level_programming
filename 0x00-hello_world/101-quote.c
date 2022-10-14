809227ee8a1f03883ae6c8316efd7fd8d17296a9
GilbertKwaku <gilbertkwaku12@gmail.com>
2022-10-14 19:36:56 +0000

    Getting started with C

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#include <stdio.h>
+i#include <stdio.h>
 #include <unistd.h>
 /**
- * Main - Entry
+ * Main - Entry point
+ * Description: 'Getting started with C'
  * Return: Always 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 49);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
