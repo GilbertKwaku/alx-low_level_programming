00ecea38300179ef28793b6044ad99adcc9b6ef8
GilbertKwaku <gilbertkwaku12@gmail.com>
2022-11-11 20:36:52 +0000

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -18,8 +18,8 @@ void _is_zero(char *argv[])
 		{
 			isn1 = 0;
 			break;
-
 		}
+
 	for (i = 0; argv[2][i]; i++)
 		if (argv[2][i] != '0')
 		{
@@ -69,6 +69,7 @@ int _checknum(char *argv[], int n)
 			printf("Error\n");
 			exit(98);
 		}
+
 	return (ln);
 }
 
@@ -91,7 +92,7 @@ int main(int argc, char *argv[])
 	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
 	if (nout == NULL)
 		printf("Error\n"), exit(98);
-	nout = _initiaiize_array(nout, lnout);
+	nout = _initialize_array(nout, lnout);
 	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
 	for (; k >= 0; k--, i--)
 	{
